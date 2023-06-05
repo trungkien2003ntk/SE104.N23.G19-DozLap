6bd54e37dbd9215620c3299d36bc1a664ea4bacc
Nguyen Kien <110883405+linhphanhoang@users.noreply.github.com>
2023-06-05 09:43:07 +0700

    Top menu + banner + list product

--- WebsiteComputers/src/app/app.component.ts
+++ WebsiteComputers/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Manufacturer } from './services/manufacturer';
+import { ManufacturerService } from './services/manufacturer.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WebsiteComputers';
+  items: MenuItem[] = [];
+  products: Manufacturer[] = [];
+  status = false;
+	
+	responsiveOptions;
+
+	constructor(private productService: ManufacturerService) { 
+		this.responsiveOptions = [
+            {
+                breakpoint: '1024px',
+                numVisible: 3,
+                numScroll: 3
+            },
+            {
+                breakpoint: '768px',
+                numVisible: 2,
+                numScroll: 2
+            },
+            {
+                breakpoint: '560px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
+	}
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then(products => {
+			this.products = products;
+		});
+
+    this.items = [
+        {
+            label:'Screen',
+            icon:'pi pi-fw pi-file',
+        },
+        {
+            label:'PC',
+            icon:'pi pi-fw pi-pencil',
+        },
+        {
+            label:'Laptop',
+            icon:'pi pi-fw pi-user',
+        },
+        {
+            label:'Keyboard',
+            icon:'pi pi-fw pi-calendar',
+        },
+    ];
+  }
 }
--- WebsiteComputers/src/app/app.module.ts
+++ WebsiteComputers/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import {ButtonModule} from 'primeng/button';
+import {CarouselModule} from 'primeng/carousel';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    //my import
+    MenubarModule,
+    ButtonModule,
+    CarouselModule,
+    HttpClientModule 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
