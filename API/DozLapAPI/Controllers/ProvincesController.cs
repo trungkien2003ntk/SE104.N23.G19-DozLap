f26999884b365f921a250e7110471ccab5754fa2
Nguyen Kien <trungkien2003ntk@gmail.com>
2023-06-20 00:07:32 +0700

    Update Controller Using AutoMapper and Add Post, put, Delete method for APIs

--- API/DozLapAPI/Controllers/ProvincesController.cs
+++ API/DozLapAPI/Controllers/ProvincesController.cs
@@ -6,42 +6,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DozLapAPI.Entities;
+using AutoMapper;
+using DozLapAPI.Models;
 
 namespace DozLapAPI.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/province")]
     public class ProvincesController : ControllerBase
     {
         private readonly DozLapDbContext _context;
+        private readonly IMapper _mapper;
 
-        public ProvincesController(DozLapDbContext context)
+        public ProvincesController(DozLapDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: api/Provinces
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Province>>> GetProvinces()
+        public async Task<ActionResult<IEnumerable<ProvinceDTO>>> GetProvinces()
         {
-            if (_context.Provinces == null)
-            {
-                return NotFound();
-            }
-
-            List<Province> provinces = await _context.Provinces.ToListAsync();
-
-            return Ok(provinces);
+            var provinces = await _context.Provinces.ToListAsync();
+            var provinceDTOs = _mapper.Map<List<ProvinceDTO>>(provinces);
+            return Ok(provinceDTOs);
         }
 
-        // GET: api/Provinces/5
+        // GET: api/Provinces/{id}
         [HttpGet("{id}")]
-        public async Task<ActionResult<Province>> GetProvince(long id)
+        public async Task<ActionResult<ProvinceDTO>> GetProvince(long id)
         {
-            if (_context.Provinces == null)
-            {
-                return NotFound();
-            }
             var province = await _context.Provinces.FindAsync(id);
 
             if (province == null)
@@ -49,92 +44,8 @@ namespace DozLapAPI.Controllers
                 return NotFound();
             }
 
-            return Ok(province);
+            var provinceDTO = _mapper.Map<ProvinceDTO>(province);
+            return Ok(provinceDTO);
         }
-
-        //// PUT: api/Provinces/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutProvince(long id, Province province)
-        //{
-        //    if (id != province.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(province).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!ProvinceExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        //// POST: api/Provinces
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<Province>> PostProvince(Province province)
-        //{
-        //  if (_context.Provinces == null)
-        //  {
-        //      return Problem("Entity set 'DozLapDbContext.Provinces'  is null.");
-        //  }
-        //    _context.Provinces.Add(province);
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateException)
-        //    {
-        //        if (ProvinceExists(province.Id))
-        //        {
-        //            return Conflict();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return CreatedAtAction("GetProvince", new { id = province.Id }, province);
-        //}
-
-        //// DELETE: api/Provinces/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteProvince(long id)
-        //{
-        //    if (_context.Provinces == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var province = await _context.Provinces.FindAsync(id);
-        //    if (province == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.Provinces.Remove(province);
-        //    await _context.SaveChangesAsync();
-
-        //    return NoContent();
-        //}
-
-        //private bool ProvinceExists(long id)
-        //{
-        //    return (_context.Provinces?.Any(e => e.Id == id)).GetValueOrDefault();
-        //}
     }
 }
--- API/DozLapAPI/Controllers/ShoppingCartItemsController.cs
+++ API/DozLapAPI/Controllers/ShoppingCartItemsController.cs
@@ -6,43 +6,44 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DozLapAPI.Entities;
+using DozLapAPI.Models;
+using AutoMapper;
 
 namespace DozLapAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/shopping_cart_item")]
     [ApiController]
     public class ShoppingCartItemsController : ControllerBase
     {
         private readonly DozLapDbContext _context;
+        private readonly IMapper _mapper;
 
-        public ShoppingCartItemsController(DozLapDbContext context)
+        public ShoppingCartItemsController(DozLapDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: api/ShoppingCartItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ShoppingCartItem>>> GetShoppingCartItems()
+        public async Task<ActionResult<IEnumerable<ShoppingCartItemDTO>>> GetShoppingCartItems()
         {
             if (_context.ShoppingCartItems == null)
             {
                 return NotFound();
             }
 
-            List<ShoppingCartItem> shoppingCartItems = await _context.ShoppingCartItems.ToListAsync();
-
-            return Ok(shoppingCartItems);
+            var shoppingCartItems = await _context.ShoppingCartItems.ToListAsync();
+            var shoppingCartItemDTOs = _mapper.Map<List<ShoppingCartItemDTO>>(shoppingCartItems);
+            return Ok(shoppingCartItemDTOs);
+            
         }
 
 
         // GET: api/ShoppingCartItems/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<ShoppingCartItem>> GetShoppingCartItem(long id)
+        public async Task<ActionResult<ShoppingCartItemDTO>> GetShoppingCartItem(long id)
         {
-            if (_context.ShoppingCartItems == null)
-            {
-                return NotFound();
-            }
             var shoppingCartItem = await _context.ShoppingCartItems.FindAsync(id);
 
             if (shoppingCartItem == null)
@@ -50,92 +51,86 @@ namespace DozLapAPI.Controllers
                 return NotFound();
             }
 
-            return Ok(shoppingCartItem);
+            var shoppingCartItemDTO = _mapper.Map<ShoppingCartItemDTO>(shoppingCartItem);
+            return Ok(shoppingCartItemDTO);
         }
 
-        //// PUT: api/ShoppingCartItems/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutShoppingCartItem(long id, ShoppingCartItem shoppingCartItem)
-        //{
-        //    if (id != shoppingCartItem.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        // PUT: api/ShoppingCartItems/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateShoppingCartItem(long id, ShoppingCartItemDTO shoppingCartItemDTO)
+        {
+            var shoppingCartItem = _mapper.Map<ShoppingCartItem>(shoppingCartItemDTO);
+            _context.Entry(shoppingCartItem).State = EntityState.Modified;
 
-        //    _context.Entry(shoppingCartItem).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShoppingCartItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!ShoppingCartItemExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+            return NoContent();
+        }
 
-        //    return NoContent();
-        //}
+        // POST: api/ShoppingCartItems
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCartItemDTO>> AddShoppingCartItem(ShoppingCartItemDTO shoppingCartItemDTO)
+        {
+            var shoppingCartItem = _mapper.Map<ShoppingCartItem>(shoppingCartItemDTO);
 
-        //// POST: api/ShoppingCartItems
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<ShoppingCartItem>> PostShoppingCartItem(ShoppingCartItem shoppingCartItem)
-        //{
-        //  if (_context.ShoppingCartItems == null)
-        //  {
-        //      return Problem("Entity set 'DozLapDbContext.ShoppingCartItems'  is null.");
-        //  }
-        //    _context.ShoppingCartItems.Add(shoppingCartItem);
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateException)
-        //    {
-        //        if (ShoppingCartItemExists(shoppingCartItem.Id))
-        //        {
-        //            return Conflict();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+            if (_context.ShoppingCartItems.Count() > 0)
+            {
+                long maxId = _context.ShoppingCartItems.Max(item => item.Id);
+                shoppingCartItem.Id = maxId + 1;
+            }
+            else
+            {
+                shoppingCartItem.Id = 1;
+            }
+            
+
+            _context.ShoppingCartItems.Add(shoppingCartItem);
+            await _context.SaveChangesAsync();
+
+            var resultDTO = _mapper.Map<ShoppingCartItemDTO>(shoppingCartItem);
+
+
+
+            return CreatedAtAction(nameof(GetShoppingCartItem), new { id = resultDTO.Id }, resultDTO);
+        }
 
         //    return CreatedAtAction("GetShoppingCartItem", new { id = shoppingCartItem.Id }, shoppingCartItem);
         //}
 
-        //// DELETE: api/ShoppingCartItems/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteShoppingCartItem(long id)
-        //{
-        //    if (_context.ShoppingCartItems == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var shoppingCartItem = await _context.ShoppingCartItems.FindAsync(id);
-        //    if (shoppingCartItem == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // DELETE: api/ShoppingCartItems/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShoppingCartItem(long id)
+        {
+            var shoppingCartItem = await _context.ShoppingCartItems.FindAsync(id);
 
-        //    _context.ShoppingCartItems.Remove(shoppingCartItem);
-        //    await _context.SaveChangesAsync();
+            if (shoppingCartItem == null)
+            {
+                return NotFound();
+            }
 
-        //    return NoContent();
-        //}
+            _context.ShoppingCartItems.Remove(shoppingCartItem);
+            await _context.SaveChangesAsync();
 
-        //private bool ShoppingCartItemExists(long id)
-        //{
-        //    return (_context.ShoppingCartItems?.Any(e => e.Id == id)).GetValueOrDefault();
-        //}
+            return NoContent();
+        }
+
+        private bool ShoppingCartItemExists(long id)
+        {
+            return (_context.ShoppingCartItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
